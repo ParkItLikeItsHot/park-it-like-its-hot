8fc37e623cdb307deb1fc4d97e132c0d6c55ec4f
simonteague6 <simon@dashf.com>
2025-10-15 17:09:25 -0500

    testing
    calculated center coordinate of the green recognition box and appended to a list.

--- opencv/car-detection.py
+++ opencv/car-detection.py
@@ -14,6 +14,15 @@ if not cap.isOpened():
     print("Error: Could not open webcam.")
     exit()
 
+
+def find_center_x(x, w):
+    return (x + (w // 2))
+
+center_x_list = [0, 0, 0]
+
+
+
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -23,12 +32,19 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect cars in the grayscale image
-    cars = car_cascade.detectMultiScale(gray, 1.1, 3)
+    cars = car_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(60, 60))
     print(f"Detected {len(cars)} cars")
     print(f"Car coordinates: {cars}")
     # Draw rectangles around the detected cars
+    # x, y is the top left corner, w, h is width and height of the rectangle
+    
+
     for (x, y, w, h) in cars:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) # Green rectangle, thickness 2
+        print(f"Center X of car: {find_center_x(x, w)}")
+        center_x_list.append(int(find_center_x(x, w)))
+        print(f"Center X list: {center_x_list}")
+    
 
     # Display the result
     cv2.imshow('Car Detection', frame)
